de7cecdd16dd94383b5aa479c804f323d194fdce
Jehan Singh <sngjeh001@myuct.ac.za>
2018-03-09 17:53:08 +0200

    Testing better quality beat splitting

---
+++ change.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+echo "remove leftovers"
+[ -e out.mp3 ] && rm out.mp3
+echo "convert to wav"
+ffmpeg -y -i $1 work.wav 2> /dev/null
+echo "determine tempo"
+aubio beat work.wav | awk '{$1=$1};1' > beatmap
+echo "Half-beating in progress"
+python improved.py
+echo "Half-beating complete! Convert to mp3"
+ffmpeg -y -i out.wav out.mp3 2> /dev/null
+echo "Cleaning up"
+rm *.wav
+echo "Done!"
+
---
+++ improved.py
@@ -0,0 +1,35 @@
+from scipy.io.wavfile import read, write
+import numpy
+import sys
+import math
+
+print("load beatmap")
+beatmap = open("beatmap","r")
+print("beatmap loaded")
+
+beattimes = beatmap.read()
+beattimes = beattimes.strip()
+beattimes = beattimes.split("\n")[:-1]
+
+print("load file")
+rate, wave = read("work.wav")
+print("file loaded")
+
+out = []
+
+print("halving things")
+
+keep = True
+for k in range(len(beattimes[:-1])):
+
+    if keep:
+        out.extend(wave[math.floor(rate*float(beattimes[k])):math.floor(rate*float(beattimes[k+1]))])
+
+    keep = not(keep)
+
+print("halving done")
+
+out = numpy.asarray(out)
+
+print("save file")
+write("out.wav", rate, out)
